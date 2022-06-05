7ac3aa32128a8436360837da446adc3dc97982c7
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-05 10:06:33 +0530

    Valid Anagram

---
+++ 19.py
@@ -0,0 +1,14 @@
+#Valid Anagram
+
+"""Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once."""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        x = [i for i in s]
+        y = [j for j in t]
+        if sorted(x) == sorted(y):
+            return True
+        else:
+            return False
