effeb658e627578eafffeb4f0fb7e84d973f009e
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-09 21:00:43 +0530

    Sum of Two Integers

---
+++ 33.py
@@ -0,0 +1,8 @@
+#Sum of Two Integers
+
+"""Given two integers a and b, return the sum of the two integers without using the operators + and -"""
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        l=[a,b]
+        return sum(l)
