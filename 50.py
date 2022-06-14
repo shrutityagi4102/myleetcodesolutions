1b4b8eee574bfb8116798baf0125763c521fe7e6
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-14 10:31:43 +0530

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,22 @@
+#Intersection of Two Arrays
+
+"""Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order."""
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums11 = list(set(nums1))
+        nums21 = list(set(nums2))
+        ans = []
+        if len(nums11) == len(nums21):
+            for i in nums11:
+                if i in nums21:
+                    ans.append(i)
+        elif len(nums11) > len(nums21):
+            for i in nums11:
+                if i in nums21:
+                    ans.append(i)
+        else:
+            for i in nums21:
+                if i in nums11:
+                    ans.append(i)
+        return ans
