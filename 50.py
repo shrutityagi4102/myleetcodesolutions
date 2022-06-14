508940e7d1f99e53834709252efc99c60ae9e7bf
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-14 10:32:16 +0530

    Intersection of Two Arrays

--- 50.py
+++ 50.py
@@ -1,6 +1,6 @@
-#Intersection of Two Arrays
+#Intersection of 2 Arrays
 
-"""Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order."""
+"""Given 2 integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order."""
 
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
