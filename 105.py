036f0b929ee6fe7ab8bc0ceb83224ecf09dcdadb
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-07-27 23:39:49 +0530

    Destination City

---
+++ 105.py
@@ -0,0 +1,13 @@
+#Destination City
+
+"""You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city."""
+
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        source = [i[0] for i in paths]
+        x = paths[0][0]
+        while x in source:
+            x = paths[source.index(x)][1]
+        return x
