4f270c43ba527c9c583972ffc8a8054f118198cc
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-08-01 15:00:42 +0530

    Kth Distinct String in an Array

---
+++ 132.py
@@ -0,0 +1,13 @@
+#Kth Distinct String in an Array
+
+"""A distinct string is a string that is present only once in an array.
+
+Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer than k distinct strings, return an empty string "".
+
+Note that the strings are considered in the order in which they appear in the array."""
+
+from collections import Counter
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        ans = [i for i in dict(Counter(arr)) if dict(Counter(arr))[i] == 1]
+        return ans[k-1] if k<=len(ans) else ""
