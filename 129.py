f2662c22424a6d71a70ba5f2ad1c77cabdee48aa
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-08-01 09:42:25 +0530

    Remove All Occurrences of a Substring

---
+++ 129.py
@@ -0,0 +1,14 @@
+#Remove All Occurrences of a Substring
+
+"""Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string."""
+
+class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        while part in s:
+            s = s.replace(part,"",1)
+        return s
