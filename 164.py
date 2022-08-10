189294e99255e68792d367e0923ddae4b9ae2a02
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-08-10 11:24:07 +0530

    Sort an Array

---
+++ 164.py
@@ -0,0 +1,7 @@
+#Sort an Array
+
+"""Given an array of integers nums, sort the array in ascending order."""
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
