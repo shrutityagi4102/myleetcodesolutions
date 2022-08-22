662d14fffd00af7fa69c975e4e71015c4ab6be76
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-08-22 23:42:55 +0530

    Detect Capital

---
+++ 177.py
@@ -0,0 +1,12 @@
+#Detect Capital
+
+"""We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right."""
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        return True if word.isupper() or word.islower() or (word[0].isupper() and word[1:].islower()) else False
