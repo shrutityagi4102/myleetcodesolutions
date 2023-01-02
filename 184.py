553307d2f075e82c828b16df1ff3c2863002c84a
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2023-01-03 00:18:10 +0530

    Cells in a Range on an Excel Sheet

---
+++ 184.py
@@ -0,0 +1,23 @@
+#Cells in a Range on an Excel Sheet
+
+"""A cell (r, c) of an excel sheet is represented as a string "<col><row>" where:
+
+<col> denotes the column number c of the cell. It is represented by alphabetical letters.
+For example, the 1st column is denoted by 'A', the 2nd by 'B', the 3rd by 'C', and so on.
+<row> is the row number r of the cell. The rth row is represented by the integer r.
+You are given a string s in the format "<col1><row1>:<col2><row2>", where <col1> represents the column c1, <row1> represents the row r1, <col2> represents the column c2, and <row2> represents the row r2, such that r1 <= r2 and c1 <= c2.
+
+Return the list of cells (x, y) such that r1 <= x <= r2 and c1 <= y <= c2. The cells should be represented as strings in the format mentioned above and be sorted in non-decreasing order first by columns and then by rows."""
+
+class Solution:
+    def cellsInRange(self, s: str) -> List[str]:
+        maxc = s[0]
+        maxc1 = chr(ord(s[-2])+1)
+        maxr = s[-1]
+        maxr1 = int(s[1])
+        ans = []
+        while maxc != maxc1:
+            for i in range(maxr1,int(maxr)+1):
+                ans.append(maxc+str(i))
+            maxc = chr(ord(maxc)+1)
+        return ans
