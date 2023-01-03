aa1e2f038ab40a0d62b7131c857583df83cb8dee
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2023-01-03 12:04:19 +0530

    Sort the People

---
+++ 193.py
@@ -0,0 +1,11 @@
+#Sort the People
+
+"""You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights."""
+
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return [x for _, x in sorted(zip(heights,names))[::-1]]
