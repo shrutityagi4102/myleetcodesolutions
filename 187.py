3199848306d2628f73fe655a2ce5a7b84ddadbc3
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2023-01-03 11:12:46 +0530

    Queries on a Permutation With Key

---
+++ 187.py
@@ -0,0 +1,19 @@
+#Queries on a Permutation With Key
+
+"""Given the array queries of positive integers between 1 and m, you have to process all queries[i] (from i=0 to i=queries.length-1) according to the following rules:
+
+In the beginning, you have the permutation P=[1,2,3,...,m].
+For the current i, find the position of queries[i] in the permutation P (indexing from 0) and then move this at the beginning of the permutation P. Notice that the position of queries[i] in P is the result for queries[i].
+Return an array containing the result for the given queries."""
+
+class Solution:
+    def processQueries(self, queries: List[int], m: int) -> List[int]:
+        ml = [i+1 for i in range(m)]
+        ans = []
+        for j in queries:
+            cop = ml.index(j)
+            cope = ml[cop]
+            ans.append(cop)
+            del ml[cop]
+            ml.insert(0,cope)
+        return ans
